206fd79b404b80cdbb7f669e44b27ea4dda937bf
Lucy Barker <lucy@geniemouse.com>
2019-08-20 16:41:04 +0100

    Store previous version for reference

--- src/js/modules/countdown.old.js
+++ src/js/modules/countdown.old.js
@@ -1,17 +1,54 @@
+/**
+ * Example usage:
+ * @todo
+ *
+ * cd.calculation()
+ * cd.status()
+ * cd.start()
+ * cd.stop()
+ * cd.pause()
+ * cd.resume()
+ *
+ */
+
+/**
+ * Time constants (commented sums to show workings)
+ * @type {Number}
+ */
 const MILLISECOND_DAY = 86400000; // 1000ms * 60s * 60mins * 24hrs
 const MILLISECOND_HOUR = 3600000; // 1000ms * 60s * 60mins
 const MILLISECOND_MINUTE = 60000; // 1000ms * 60s
 const MILLISECOND_SECOND = 1000; // 1000ms * 1s
 
+// @todo:
+// Perhaps a elements hook function???
+
+function fn(value) {
+    return function() {
+        return value;
+    };
+}
+
+function stopTimer() {
+    clearTimeout(this.timerId());
+    this.timerId = fn(null);
+}
+
+function addZero(units) {
+    Object.keys(units).map((unit) => {
+        const value = Number(units[unit]);
+        return (units[unit] = value >= 0 && value < 10 ? `0${value}` : `${value}`);
+    });
+
+    return units;
+}
+
 /**
- * Recursive function: get millisecond time value broken down by units of time
- * @param  {Number} time      -- Time in milliseconds
- * @param  {Number} remainder -- (Optional) Remaiing millisecond value after current unit calculation.
- *                                 Added on recursive calls
- * @param  {Number} index     -- (Optional) Added on recursive calls
- * @return {Object}           -- e.g. { "days": 148, "hours": 12, "minutes": 34, "seconds": 40 }
+ * Get the countdown time as an object of units: days, hours, minutes, seconds
+ * @param  {Number} time -- Time in milliseconds
+ * @return {Object}      -- e.g. { target: 123456789, "days": 1, "hours": 10, "minutes": 17, "seconds": 36 }
  */
-export function getCountDownUnits(time) {
+function getData(time) {
     const units = [
         { name: "days", value: MILLISECOND_DAY },
         { name: "hours", value: MILLISECOND_HOUR },
@@ -21,7 +58,15 @@ export function getCountDownUnits(time) {
 
     const result = {};
 
-    function cdUnits(time, remainder, index = 0) {
+    /**
+     * Recursive function: get millisecond time value broken down by units of time
+     * @param  {Number} time      -- Time in milliseconds
+     * @param  {Number} remainder -- (Optional) Remaining millisecond value after current unit calculation.
+     *                                 Added on recursive calls
+     * @param  {Number} index     -- (Optional) Added on recursive calls
+     * @return {Object}           -- e.g. { target: 123456789, "days": 1, "hours": 10, "minutes": 17, "seconds": 36 }
+     */
+    function calculateUnits(time, remainder, index = 0) {
         const unit = units[index].value;
         const nextIndex = index + 1;
 
@@ -31,46 +76,134 @@ export function getCountDownUnits(time) {
             result.target = time;
         }
 
-        const unitResult = Math.floor(remainder / unit);
+        const division = remainder / unit;
+        const unitResult = division >= 0 ? Math.floor(division) : 0;
         result[units[index].name] = unitResult;
 
         if (nextIndex < units.length) {
-            cdUnits(unitResult, remainder % unit, nextIndex);
+            calculateUnits(unitResult, remainder % unit, nextIndex);
         }
     }
 
-    cdUnits(time);
+    calculateUnits(time);
     return result;
 }
 
-export function startCountDown(time, options = {}) {
-    const { interval = MILLISECOND_SECOND, func = new Function() } = options;
-    let data = getCountDownUnits(time);
+/**
+ * Start countdown, exposed as `COUNTDOWN_INSTANCE.start()`
+ * @param  {Number} time    -- Time in milliseconds
+ * @param  {Object} options -- (Optional) Defaults: { interval: 1000, onStep: () => {} }
+ * @return {Undefined}
+ */
+function startCountDown(time, options = {}) {
+    const { interval, onStep, zeroBased } = options;
+    let data = getData(time);
 
     function hasTimeLeft() {
         return data.target / interval > 1;
     }
 
-    // Countdown is already over, so let's get out of here!
-    if (!hasTimeLeft()) {
+    // Quick get-out clauses:
+    // Countdown is already over, or timer already in progress
+    if (!hasTimeLeft() || this.timerId()) {
         return;
     }
 
-    function tick() {
-        let timer = setTimeout(tick, interval);
-        if (hasTimeLeft()) {
-            data.target -= interval;
+    (function tick(context) {
+        // Using nested `setTimeout` calls instead of `setInterval` for efficiency;
+        // more breathing space for garbage collection, etc
+        const timer = setTimeout(tick, interval, context);
 
-            if (data.seconds > 0) {
-                data.seconds -= 1;
-            } else {
-                data = getCountDownUnits(data.target);
-            }
-            return func.call(null, data);
-        } else {
-            clearTimeout(timer);
+        if (!hasTimeLeft()) {
+            stopTimer.call(context);
+            return;
         }
+
+        // Manually countdown seconds; avoids need need to call `getData(...)`
+        // (four recursive calls) every second
+        data.target -= interval;
+
+        if (data.seconds > 0) {
+            data.seconds -= 1;
+        } else {
+            // Call `getData(...)` once a minute
+            data = getData(data.target);
+        }
+
+        // Expose countdown globally useful information
+        // Return as functions, so the actual values are kept private
+        context.timerId = fn(timer);
+        context.status = fn(zeroBased ? addZero(data) : data);
+
+        // Make the callback
+        onStep.call(null, data);
+    })(this);
+
+    return data;
+}
+
+/**
+ * Stop the countdown
+ * @param  {Object} options     -- (Optional) Defaults: { onReset: () => {}, reset: false }
+ * @return {Undefined|Object}
+ */
+function stopCountDown(options = {}) {
+    const { reset, onReset } = options;
+    stopTimer.call(this);
+    if (reset) {
+        return onReset.call(null, getData(0));
+    }
+}
+
+/**
+ * Countdown API wrapper
+ * @param  {Number} milliseconds    -- Time in milliseconds
+ * @return {Object}                 -- cf. Example usage section at file top
+ */
+export default function countDown(milliseconds, options = {}) {
+    const countDownSettings = {
+        interval: MILLISECOND_SECOND,
+        zeroBased: true,
+        onStep: () => {},
+        onReset: (data) => data,
+        ...options
+    };
+    let pausedTimeStamp;
+
+    function units() {
+        return getData.call(this, milliseconds);
     }
 
-    tick();
+    function start() {
+        return startCountDown.call(this, milliseconds, countDownSettings);
+    }
+
+    function stop(options = {}) {
+        const { reset = true } = options;
+        const { onReset } = countDownSettings;
+        return stopCountDown.call(this, {
+            onReset,
+            reset
+        });
+    }
+
+    function pause() {
+        const currentTimeRemaining = this.status().target;
+        stopTimer.call(this);
+        pausedTimeStamp = currentTimeRemaining;
+    }
+
+    function resume() {
+        return startCountDown.call(this, pausedTimeStamp, countDownSettings);
+    }
+
+    return {
+        calculation: fn(units()),
+        status: fn({}),
+        timerId: fn(null),
+        pause,
+        resume,
+        start,
+        stop
+    };
 }
